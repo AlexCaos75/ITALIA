3646100ace0b6def0ea602d1f90fb1dcc82998b6
AlexCaos75 <alessandro75.ar@gmail.com>
2025-12-30 15:19:09 +0100

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,8 +1,18 @@
+// assets/js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
 export const firebaseConfig = {
-  apiKey: "INSERISCI_API_KEY",
-  authDomain: "INSERISCI_AUTH_DOMAIN",
-  projectId: "INSERISCI_PROJECT_ID",
-  storageBucket: "INSERISCI_STORAGE_BUCKET",
-  messagingSenderId: "INSERISCI_MESSAGING_SENDER_ID",
-  appId: "INSERISCI_APP_ID"
+  apiKey: "AIzaSyC4wjxtURhqn1cfiaEDWXSLrj9-BgwoINs",
+  authDomain: "quiz-regioni.firebaseapp.com",
+  databaseURL: "https://quiz-regioni-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "quiz-regioni",
+  storageBucket: "quiz-regioni.firebasestorage.app",
+  messagingSenderId: "80504945646",
+  appId: "1:80504945646:web:865dac2c7890c3a62b2cff"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
